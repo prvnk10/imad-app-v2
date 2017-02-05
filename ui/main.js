67b34d86b63f4e52a889bb06afdfaeeef462a0c1
prvnk10 <prvnk10@gmail.com>
2017-02-05 08:48:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,19 @@
-console.log('Loaded!');
+//console.log('Loaded!');
+
+var comment = document.getElementById('comment');
+var submit = document.getElementById('submit_btn');
+
+submit.onclick = function(){
+    var xhttp = new XMLHTTPRequest();
+    
+    xhttp.onreadystatechange = function(){
+        if(xhttp.readyState == XMLHttpRequest.DONE && xhttp.status == 200){
+            var comment_box = document.getElementById('showComments');
+            comment_box.innerHTML = xhttp.responseText;
+            
+        }
+    };
+    
+    xhttp.open('GET', 'http://prvnk10.imad.hasura-app.io/comment', true);
+    xhttp.send(null);
+};
