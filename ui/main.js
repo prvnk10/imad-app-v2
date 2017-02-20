c4b8de76f3e6ffa1b3e6b868a15df678a542d6c5
Parveen Khurana <Parveen Khurana>
2017-02-20 13:13:00 +0530

    changes made to main.js file

--- ui/main.js
+++ ui/main.js
@@ -1,51 +1,14 @@
 console.log('Loaded!');
 
-/*
-var button = document.getElementById('submit_btn');
-button.onclick = function(){
+function tryThis(){
+  var xhttp = new XMLHttpRequest();
 
-  var xhttp = new XMLHTTPRequest();
-    
   xhttp.onreadystatechange = function(){
   if(xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200){
-            
-    var comments = xhttp.responseText;
-    comments = JSON.parse(comments);
-            
-    for(var i=0; i<comments.length ; i++){
-     list += '<li>' + comments[i] + '</li>';
+      var span = document.getElementById('commentValue');
+      span.innerHTML = xhttp.responseText;
     }
-            
-    var comment_box = document.getElementById('showComments');
-    comment_box.innerHTML = list;
-            
-    }
-  };
-    
-  var commentInput = document.getElementById('comment');
-  commment = commentInput.value;
-  xhttp.open('GET', 'http://prvnk10.imad.hasura-app.io/comment?c=' + comment, true);
-  xhttp.send(null);
-
-};  */
-
-var button = document.getElementById('count');
-button.onclick = function(){
-    
-    
-  var xhttp = new XMLHTTPRequest();
-    
-  xhttp.onreadystatechange = function(){
-  if(xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200){
-    
-    var counterValue = xhttp.responseText;
-    var comment_box = document.getElementById('counter');
-    comment_box.innerHTML = counterValue;
-    }
-  };
-    
-  xhttp.open('GET', 'http://prvnk10.imad.hasura-app.io/counter', true);
-  xhttp.send(null);
-    
-};
-
+ };
+ xhttp.open('GET', '/counter', true);
+ xhttp.send(null);
+}
