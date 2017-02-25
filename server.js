c680d097aea2192b083567291b46cf9d28cc952f
prvnk10 <prvnk10@gmail.com>
2017-02-25 21:19:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,13 +97,26 @@ app.get('/articles/:articleName', function(req,res){
             if(result.rows.length === 0){
                 res.status(404).send('Article not found');
             } else {
-                var articleData = result.rows[1];
+                var articleData = result.rows[0];
                 res.send(createTemplate(articleData));
             }
         }
           
       });
 });
+
+
+var pool = new Pool(config);
+app.get('/test-db' , function(req,res){
+   pool.query("SELECT * FROM test", function(err, result){
+      if(err){
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result));
+      }
+   });
+});
+
 
 
 app.get('/ui/style.css', function (req, res) {
