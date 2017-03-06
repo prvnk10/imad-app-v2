702f6b098de274cc4d85e1d9db0fc67a88271b78
prvnk10 <prvnk10@gmail.com>
2017-03-06 12:11:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
+
 
 var config = {
     user: 'prvnk10',
@@ -14,6 +16,8 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+
+app.use(bodyParser.json());
 
 var articles = {
   'article-one': { title: 'Article One | Parveen Khurana', heading : 'Article One', date : 'Feb 4, 2017', content: `
@@ -76,6 +80,27 @@ function hash(input, salt){
 app.get('/hash/:input', function(req,res){
    var hashedString = hash(req.params.input, 'this-is-some-random-string');
    res.send(hashedString);
+});
+
+var pool = new Pool(config);
+app.post('/create-user', function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    var salt = crypto.randomBytes(128).toString('hex');
+    var dbString = hash(password, salt);
+    
+    pool.query('INSERT INTO "user"(username, password) VALUES ($1, $2)' , [username, dbString], function(err, result){
+       
+       if(err)
+       {
+           res.status(500).send(err.toString());
+       }
+       else
+       {
+           res.send('User successfully created' + username);
+       }
+    });
 });
 
 var counter = 0;
