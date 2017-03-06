ceae2c8d079479ba71ab32641c2751b77172a524
prvnk10 <prvnk10@gmail.com>
2017-03-06 12:20:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.post('/create-user', function(req,res){
        }
        else
        {
-           res.send('User successfully created' + username);
+           res.send('User successfully created ' + username);
        }
     });
 });
