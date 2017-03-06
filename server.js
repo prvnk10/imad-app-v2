58b8010965c616eb5fdbb67d21ba27451cc11681
prvnk10 <prvnk10@gmail.com>
2017-03-06 12:27:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,6 +103,42 @@ app.post('/create-user', function(req,res){
     });
 });
 
+app.post('/login', function(req,res){
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   pool.query('SELECT * FROM "users" WHERE username=$1', [username], function(err,result){
+      if(err)
+      {
+          res.status(500).send(err.toString());
+      }
+      else
+      {
+          if(result.rows.length === 0)
+          {
+              res.status(403).send('username/password is invalid');
+          }
+          else
+          {
+              // match the password
+              var dbString = result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              
+              var hashedPassword = hash(password, salt);
+              if(hashedPassword === dbString)
+              {
+                  // set session
+                  res.send("successfully logged in!");
+              }
+              else
+              {
+                  res.status(403).send('username/password is invalid');
+              }
+          }
+      }
+   });
+});
+
 var counter = 0;
 app.get('/counter', function(req,res){
   counter += 1;
