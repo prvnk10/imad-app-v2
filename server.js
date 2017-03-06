d465684193bc9c5040625477e9f0ef0f27b19fb4
prvnk10 <prvnk10@gmail.com>
2017-03-06 08:22:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -137,16 +137,6 @@ app.get('/ui/bootstrap.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bootstrap.js'));
 });
 
-function hash(input, salt){
-  var hashed = hash(input, salt, 1000, 512, 'sha512');
-  return hashed.toString('hex');
-}
-
-app.get('/hash/:input', function(req,res){
-   var hashedString = hash(req.params.input, 'this-is-some-random-string');
-   res.send(hashedString);
-});
-
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
