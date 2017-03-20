59605ebf30905e8dc1d3a89b12ff57f0566dff6c
prvnk10 <prvnk10@gmail.com>
2017-03-20 19:14:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,3 +12,32 @@ function tryThis(){
  xhttp.open('GET', '/counter', true);
  xhttp.send(null);
 }
+
+
+function submitComment(){
+
+  var xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function(){
+  if(xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200){
+
+    var comments = xhttp.responseText;
+    comments = JSON.parse(comments);
+
+     var list = '<li> </li>';
+    for(var i=0; i<comments.length ; i++){
+     list += '<li>' + comments[i] + '</li>';
+    }
+
+    var comment_box = document.getElementById('showComments');
+    comment_box.innerHTML = list;
+
+    }
+  };
+
+  var commentInput = document.getElementById('comment');
+  comment = commentInput.value;
+  xhttp.open('GET', '/comment?c=' + comment, true);
+  xhttp.send(null);
+
+}
