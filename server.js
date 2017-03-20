a315c451a61c05d67e3ee42042513ef2c5e7ae41
prvnk10 <prvnk10@gmail.com>
2017-03-20 19:53:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,12 +82,13 @@ function createTemplate(data){
     </div>
     
     <div> ${content} </div>
+    
     <div>
-    <input type='text' id='comment'> </input>
-    <input type='submit' name='submit' value='submit' id='submit_btn' onclick="submitComment()">
-    </input> 
-    <span id='showComments'> <ul></ul> </span>
+     <input type='text' id='comment'>
+     <input type='submit' name='submit' value='submit' id='submit_btn' onclick="submitComment()">
+     <span id='showComments'> <ul> </ul> </span>
     </div>
+    
     </body>
     </html>
   `;
